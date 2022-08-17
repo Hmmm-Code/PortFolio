a08883bba80d9d9d546bcd110bc4a3521dac0eef
“Khan” <skcbskcb0@gmail.com>
2022-08-17 22:00:51 +0400

    optimze some code and setup mongdb and mongoos

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const user = JSON.parse(fs.readFileSync(`${__dirname}/../data/userData.json`));
-exports.app;
+
 app.get("/api/v1/user", (req, res) => {
   res.status(200).json({
     status: "sucess",
