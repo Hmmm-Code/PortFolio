b5838e390bf17cc8761c9d4be6a8068dcf0a56ad
“Khan” <skcbskcb0@gmail.com>
2022-08-17 21:58:43 +0400

    optimize some code and setup mangodb and mangoos

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
+const fs = require("fs");
 const express = require("express");
-
 const app = express();
-
-app.get(".api/v1/user", (req, res) => {
-  res.status(200);
-});
-
-const port = 8080;
-app.listen(port, () => {
-  console.log(`App is runung on ${port} ...`);
+const user = JSON.parse(fs.readFileSync(`${__dirname}/../data/userData.json`));
+exports.app;
+app.get("/api/v1/user", (req, res) => {
+  res.status(200).json({
+    status: "sucess",
+    data: {
+      user,
+    },
+  });
 });
