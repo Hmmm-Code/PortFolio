3f8690fd9dc41d832451289fd254ab56f1327faf
“Khan” <skcbskcb0@gmail.com>
2022-08-26 20:28:35 +0400

    implement the skill levels in skill component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,20 @@
+import { width } from "@mui/system";
 import React from "react";
 
 const Skills = () => {
+  const skills = [
+    { name: "javaScript", level: "85%", style: { width: "85% " } },
+    { name: "React.JS", level: "85%", style: { width: "85% " } },
+    { name: "Node.JS", level: "60%", style: { width: "60% " } },
+    { name: "Express.JS", level: "60%", style: { width: "60% " } },
+    { name: "MongoDB", level: "60%", style: { width: "60% " } },
+    { name: "Figma", level: "60%", style: { width: "60% " } },
+    { name: "JAVA", level: "50%", style: { width: "50% " } },
+    { name: "Python", level: "50%", style: { width: "50% " } },
+  ];
+  const style = {
+    width: "10%",
+  };
   // implement the feature tu adjust skills level dynamically
   return (
     <section className="ftco-section" id="skills-section">
@@ -13,166 +27,25 @@ const Skills = () => {
           </div>
         </div>
         <div className="row">
-          <div className="col-md-6 animate-box">
-            <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
-              <h3>JavaScript</h3>
-              <div className="progress">
-                <div
-                  className="progress-bar color-1"
-                  role="progressbar"
-                  aria-valuenow="90"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span>90%</span>
+          {skills.map((items) => (
+            <div className="col-md-6 animate-box">
+              <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
+                <h3>{items.name}</h3>
+                <div className="progress">
+                  <div
+                    className="progress-bar color-1"
+                    role="progressbar"
+                    aria-valuenow="90"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={items.style}
+                  >
+                    <span>{items.level}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-md-6 animate-box">
-            <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
-              <h3>React Js</h3>
-              <div className="progress">
-                <div
-                  className="progress-bar color-2"
-                  role="progressbar"
-                  aria-valuenow="85"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span>85%</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 animate-box">
-            <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
-              <h3>Node JS</h3>
-              <div className="progress">
-                <div
-                  className="progress-bar color-3"
-                  role="progressbar"
-                  aria-valuenow="95"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span>95%</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 animate-box">
-            <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
-              <h3>Express JS</h3>
-              <div className="progress">
-                <div
-                  className="progress-bar color-4"
-                  role="progressbar"
-                  aria-valuenow="90"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span>90%</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 animate-box">
-            <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
-              <h3>Mango DB</h3>
-              <div className="progress">
-                <div
-                  className="progress-bar color-5"
-                  role="progressbar"
-                  aria-valuenow="70"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span>70%</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 animate-box">
-            <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
-              <h3>SEO</h3>
-              <div className="progress">
-                <div
-                  className="progress-bar color-6"
-                  role="progressbar"
-                  aria-valuenow="80"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span>80%</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 animate-box">
-            <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
-              <h3>Figma</h3>
-              <div className="progress">
-                <div
-                  className="progress-bar color-6"
-                  role="progressbar"
-                  aria-valuenow="80"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span>80%</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 animate-box">
-            <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
-              <h3>Java</h3>
-              <div className="progress">
-                <div
-                  className="progress-bar color-6"
-                  role="progressbar"
-                  aria-valuenow="80"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span>80%</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 animate-box">
-            <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
-              <h3>Python</h3>
-              <div className="progress">
-                <div
-                  className="progress-bar color-6"
-                  role="progressbar"
-                  aria-valuenow="80"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span>80%</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 animate-box">
-            <div className="progress-wrap ftco-animate fadeInUp ftco-animated">
-              <h3>MUI</h3>
-              <div className="progress">
-                <div
-                  className="progress-bar color-6"
-                  role="progressbar"
-                  aria-valuenow="80"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  <span>80%</span>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
